23b1aaa52c1ae61b9d5948a12c433cd9a3d2e42b
yolisses <iamurise@gmail.com>
2023-09-22 16:43:22 -0300

    load internships from mock

--- frontend/src/internship/internshipsPage.tsx
+++ frontend/src/internship/internshipsPage.tsx
@@ -1,22 +1,28 @@
+import { useEffect, useState } from "react";
+import { api } from "../api/api";
+import { Internship } from "../types/internship";
 import { InternshipsItem } from "./internshipItem";
 
 export function InternshipsPage() {
+  const [internships, setInternships] = useState<Internship[]>();
+
+  async function getInternships() {
+    const res = await api.get<Internship[]>("/internships");
+    setInternships(res.data);
+  }
+
+  useEffect(() => {
+    getInternships();
+  }, []);
+
+  // TODO replace this
+  if (!internships) return <div>loading</div>;
+
   return (
     <div className="items-center p-2">
       <div className="md:grid md:grid-cols-2 lg:grid-cols-3 gap-2 w-full max-w-4xl">
-        {[...Array(20)].map((index) => {
-          return (
-            <InternshipsItem
-              internship={{
-                id: "randomId",
-                position: "Rockstar",
-                enterprise: {
-                  name: "Sonhin",
-                  picture: "/enterprise mock cover.svg",
-                },
-              }}
-            ></InternshipsItem>
-          );
+        {internships.map((internship) => {
+          return <InternshipsItem internship={internship} />;
         })}
       </div>
     </div>
