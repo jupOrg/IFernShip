7673df8949d9567b1145797388b93c78a47ee84d
yolisses <iamurise@gmail.com>
2023-11-01 16:48:46 -0300

    fix searchInput style

--- frontend/src/internship/searchInput.tsx
+++ frontend/src/internship/searchInput.tsx
@@ -1,15 +1,18 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { FaSearch } from "react-icons/fa";
 
 export function SearchInput() {
   return (
-    <div>
-      <FontAwesomeIcon className="bg-red-500" icon={["fal", "coffee"]} />
-      <FontAwesomeIcon icon={["far", "coffee"]} />
-      <FontAwesomeIcon icon={["fas", "coffee"]} />
-      <FontAwesomeIcon icon="coffee" />
-      <FontAwesomeIcon icon={["fab", "github"]} />
-
-      <input type="text" name="" id="" className="default-input rounded-full" />
+    <div className="relative items-center flex-row">
+      <FaSearch
+        size={20}
+        className="absolute left-3 opacity-50 pointer-events-none"
+      />
+      <input
+        type="text"
+        name=""
+        id=""
+        className="default-input rounded-full pl-9"
+      />
     </div>
   );
 }
