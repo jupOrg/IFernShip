b573c6f8015bb4b6ec64a10b9e5c65375c011206
yolisses <iamurise@gmail.com>
2023-11-01 15:34:50 -0300

    use gradientCurve on internshipPage

--- frontend/src/internship/internshipPage.tsx
+++ frontend/src/internship/internshipPage.tsx
@@ -1,10 +1,12 @@
 import { useLoaderData } from "react-router-dom";
+import { GradientCurve } from "../common/gradientCurve";
 import { Internship } from "../types/internship";
 
 export function InternshipPage() {
   const internship = useLoaderData() as Internship;
   return (
     <div className="items-center p-2">
+      <GradientCurve />
       <div className="w-full max-w-xl gap-4">
         <h1 className="font-semibold text-xl text-green-700">
           {internship.enterprise.name}
@@ -19,7 +21,7 @@ export function InternshipPage() {
         <h1 className="font-semibold text-xl text-green-700">
           Sobre a empresa
         </h1>
-        <div>{internship.enterprise.description}</div>
+        <div>{internship.enterprise.description}</div>G
       </div>
     </div>
   );
