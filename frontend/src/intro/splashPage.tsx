90994dc68cd2b30b03ee96cd887d8e09be5a8fb5
yolisses <iamurise@gmail.com>
2023-11-10 09:15:09 -0300

    add splash animation

--- frontend/src/intro/splashPage.tsx
+++ frontend/src/intro/splashPage.tsx
@@ -1,11 +1,11 @@
 export function SplashPage() {
   return (
     <div className="min-h-screen to-green-500 from-cyan-500 bg-gradient-to-b justify-center items-center">
-      <div className="gap-24 items-center justify-around">
+      <div className="gap-24 items-center justify-around animate-pulse">
         <h1 className="text-white text-4xl font-bold text-center hidden sm:flex">
           IFernShip
         </h1>
-        <img src="/ifpb-logo-white.svg" alt="IFPB" width={150} />
+        <img alt="IFPB" width={150} src="/ifpb-logo-white.svg" />
       </div>
     </div>
   );
