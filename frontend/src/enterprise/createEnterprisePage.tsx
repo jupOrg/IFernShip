9916a0bacd5b4c72a926db7e42292a497d9c7f6e
yolisses <iamurise@gmail.com>
2023-11-11 09:17:35 -0300

    add enterprise form submit

--- frontend/src/enterprise/createEnterprisePage.tsx
+++ frontend/src/enterprise/createEnterprisePage.tsx
@@ -1,8 +1,20 @@
+import { useForm } from "react-hook-form";
+import { api } from "../api/api";
 import { GoBackArrow } from "../common/goBackArrow";
 import { GradientCurve } from "../common/gradientCurve";
 import { ImageInput } from "../common/imageInput";
+import { Enterprise } from "../types/enterprise";
+
+type FieldValues = Omit<Enterprise, "picture">;
 
 export function CreateEnterprisePage() {
+  const { register, handleSubmit } = useForm<FieldValues>();
+
+  async function submit(fields: FieldValues) {
+    const res = await api.post("/enterprises", fields);
+    console.log(res.data);
+  }
+
   return (
     <div className="items-center">
       <GradientCurve />
@@ -11,15 +23,31 @@ export function CreateEnterprisePage() {
           <GoBackArrow to="/estagios" />
           Cadastrar empresa
         </h1>
-        <form className="gap-2 flex flex-col">
-          <input type="text" className="default-input" placeholder="Nome" />
+        <form className="gap-2 flex flex-col" onSubmit={handleSubmit(submit)}>
+          <input
+            type="text"
+            placeholder="Nome"
+            className="default-input"
+            {...register("name")}
+          />
           <textarea
             rows={3}
             className="default-input"
             placeholder="Sobre a empresa"
+            {...register("description")}
           />
-          <input type="text" className="default-input" placeholder="CNPJ" />
-          <input type="email" className="default-input" placeholder="E-mail" />
+          <input
+            type="text"
+            placeholder="CNPJ"
+            className="default-input"
+            {...register("cnpj")}
+          />
+          <input
+            type="email"
+            className="default-input"
+            placeholder="E-mail"
+            {...register("email")}
+          />
           <ImageInput />
           <button type="submit" className="default-submit">
             Cadastrar
--- frontend/src/types/enterprise.d.ts
+++ frontend/src/types/enterprise.d.ts
@@ -1,5 +1,7 @@
 export type Enterprise = {
   name: string;
+  cnpj: string;
+  email: string;
   picture: string;
   description: string;
 };
