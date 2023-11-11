02ab666c787a87f84c48ef9fa3937a7c43781e6c
yolisses <iamurise@gmail.com>
2023-11-11 09:05:05 -0300

    add createEnterprise form

--- frontend/src/enterprise/createEnterprisePage.tsx
+++ frontend/src/enterprise/createEnterprisePage.tsx
@@ -1,15 +1,31 @@
 import { GoBackArrow } from "../common/goBackArrow";
 import { GradientCurve } from "../common/gradientCurve";
+import { ImageInput } from "../common/imageInput";
 
 export function CreateEnterprisePage() {
   return (
     <div className="items-center">
       <GradientCurve />
-      <div className="page-header">
-        <GoBackArrow to="/empresas" />
-        Cadastrar empresa
+      <div className="w-full max-w-xl gap-6">
+        <h1 className="page-header">
+          <GoBackArrow to="/estagios" />
+          Cadastrar empresa
+        </h1>
+        <form className="gap-2 flex flex-col">
+          <input type="text" className="default-input" placeholder="Nome" />
+          <textarea
+            rows={3}
+            className="default-input"
+            placeholder="Sobre a empresa"
+          />
+          <input type="text" className="default-input" placeholder="CNPJ" />
+          <input type="email" className="default-input" placeholder="E-mail" />
+          <ImageInput />
+          <button type="submit" className="default-submit">
+            Cadastrar
+          </button>
+        </form>
       </div>
-      <div className="w-full max-w-xl gap-6">a</div>
     </div>
   );
 }
