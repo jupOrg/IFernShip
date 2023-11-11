d5ac9481602f4c14346b7e93cb9e1e964d8386d8
yolisses <iamurise@gmail.com>
2023-11-11 10:09:48 -0300

    bring navBar back to enterprisesPage

--- frontend/src/enterprise/enterprisesPage.tsx
+++ frontend/src/enterprise/enterprisesPage.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { api } from "../api/api";
 import { GradientCurve } from "../common/gradientCurve";
+import { NavBar } from "../nav/navBar";
 import { TopBar } from "../nav/topBar";
 import { Enterprise } from "../types/enterprise";
 import { EnterprisesItem } from "./enterpriseItem";
@@ -22,7 +23,7 @@ export function EnterprisesPage() {
 
   return (
     <div className="flex flex-row">
-      {/* <NavBar /> */}
+      <NavBar />
       <div className="items-center p-2 gap-4 flex-1">
         <TopBar />
         <GradientCurve />
