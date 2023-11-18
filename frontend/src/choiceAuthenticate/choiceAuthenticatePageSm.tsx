7dd2ce920605f59b144ef38b5ad6f1a3625bae67
Pedro <pedrojuniorifpb@gmail.com>
2023-11-18 19:43:57 -0300

    Styling mobile page from choiceAuthenticate

--- frontend/src/choiceAuthenticate/choiceAuthenticatePageSm.tsx
+++ frontend/src/choiceAuthenticate/choiceAuthenticatePageSm.tsx
@@ -1,23 +1,22 @@
 export function ChoiceAuthenticateSM() {
   return (
-    <div className="min-h-screen to-green-500 from-cyan-500 bg-gradient-to-b justify-between items-center p-4">
-      <picture className="w-full flex sm:justify-center">
+    <div className="min-h-screen gradient-page items-center p-4 gap-40">
+      <picture className="w-full flex">
         <img src="./ifpb-logo-white.svg" alt="logo" className="w-[6.5rem]" />
       </picture>
-      <div className="gap-2 items-center max-w-xl">
-        <h1 className="font-bold text-3xl">Vagas de Estágios</h1>
-        {/* TODO replace the "baixe agora" */}
-        <div>
+      <div className="gap-2 items-center max-w-lg">
+        <h1 className="font-bold text-5xl">Vagas de Estágios</h1>
+        <p className="text-black text-2xl text-center">
           Encontre o estágio dos seus sonhos e comece a trilhar o caminho para o
           sucesso profissional! Não perca mais tempo procurando vagas
           manualmente, baixe agora e comece a construir seu futuro!
-        </div>
+        </p>
       </div>
-      <div className="flex-row gap-2 max-w-4xl">
-        <button className="bg-blue-500 text-white rounded-full py-2 px-4">
+      <div className="flex-row gap-4 w-full max-w-lg">
+        <button className="btn btn-primary py-4">
           Entrar
         </button>
-        <button className="bg-gray-500 text-white rounded-full py-2 px-4">
+        <button className="btn btn-secondary py-4">
           Registrar-se
         </button>
       </div>
