d71a68fdeda54b1cabd4b4987565d8a8f1e52627
Pedro <pedrojuniorifpb@gmail.com>
2023-11-18 18:27:01 -0300

    Add new Logo for small pages

--- frontend/src/choicePerson/choicePersonPageSm.tsx
+++ frontend/src/choicePerson/choicePersonPageSm.tsx
@@ -1,11 +1,12 @@
 import { CardChoicePerson } from "./choicePersonCard";
-import { Logo } from "../common/logo";
 
 export function ChoicePersonSM() {
   return (
-    <div className="min-h-screen items-center gradient-page gap-28">
-      <Logo />
-      
+    <div className="min-h-screen items-center gradient-page gap-28 p-6">
+      <picture className="w-full flex sm:justify-center">
+        <img src="./ifpb-logo-white.svg" alt="logo" className="w-[6.5rem]" />
+      </picture>
+
       <span className="text-white text-center">
         <h1 className="font-bold text-6xl">Quem é Você?</h1>
         <p className="font-light mt-4 text-4xl w-96">
--- frontend/src/common/logoutButton.tsx
+++ frontend/src/common/logoutButton.tsx
@@ -1,11 +1,14 @@
 import { FaDoorOpen } from "react-icons/fa";
 import { useAuth } from "../auth/authContext";
+import { useNavigate } from "react-router-dom";
 
 export function LogoutButton() {
   const { logout } = useAuth();
+  const navigate = useNavigate()
 
-  function handleClick() {
+  async function handleClick() {
     logout();
+    navigate("/");
   }
 
   return (
