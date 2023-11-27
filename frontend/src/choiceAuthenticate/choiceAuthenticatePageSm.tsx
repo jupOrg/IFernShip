dfa7d9a836cc1e531f4189c8e71a44a77764ee73
yolisses <iamurise@gmail.com>
2023-11-27 19:08:26 -0300

    remove font-size variance

--- frontend/src/choiceAuthenticate/choiceAuthenticatePageSm.tsx
+++ frontend/src/choiceAuthenticate/choiceAuthenticatePageSm.tsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 export function ChoiceAuthenticateSM() {
   return (
     <div className="min-h-screen gradient-page items-center p-4 gap-40">
@@ -5,20 +7,18 @@ export function ChoiceAuthenticateSM() {
         <img src="./ifpb-logo-white.svg" alt="logo" className="w-[6.5rem]" />
       </picture>
       <div className="gap-2 items-center max-w-lg">
-        <h1 className="font-bold text-5xl">Vagas de Estágios</h1>
-        <p className="text-black text-2xl text-center">
+        <h1 className="font-bold text-2xl">Vagas de Estágios</h1>
+        <p className="text-black text-center">
           Encontre o estágio dos seus sonhos e comece a trilhar o caminho para o
           sucesso profissional! Não perca mais tempo procurando vagas
           manualmente, baixe agora e comece a construir seu futuro!
         </p>
       </div>
-      <div className="flex-row gap-4 w-full max-w-lg">
-        <button className="btn btn-primary py-4">
+      <div className="w-full gap-4 max-w-xl">
+        <Link to={"/entrar"} className="button btn btn-primary py-4">
           Entrar
-        </button>
-        <button className="btn btn-secondary py-4">
-          Registrar-se
-        </button>
+        </Link>
+        <button className="btn btn-secondary py-4">Registrar-se</button>
       </div>
     </div>
   );
