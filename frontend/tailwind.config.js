700721e496448546b221160f1e9fa90f75bf9472
yolisses <iamurise@gmail.com>
2023-11-27 14:47:37 -0300

    fix warn Instead of /public/gradient-curve-right.svg, use /gradient-curve-right.svg.

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,18 +4,18 @@ export default {
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-left-img': "url('/public/gradient-curve-left.svg')",
-        'gradient-right-img': "url('/public/gradient-curve-right.svg')",
+        "gradient-left-img": "url('/gradient-curve-left.svg')",
+        "gradient-right-img": "url('/gradient-curve-right.svg')",
       },
       backgroundSize: {
-        'gradient-left': '50vmax 100%',
-        'gradient-left-static': '10rem 100%',
-        'gradient-right': '65vmax calc(100% + 10rem)',
-        'gradient-right-static': '60rem calc(100% + 10rem)',
+        "gradient-left": "50vmax 100%",
+        "gradient-left-static": "10rem 100%",
+        "gradient-right": "65vmax calc(100% + 10rem)",
+        "gradient-right-static": "60rem calc(100% + 10rem)",
       },
       screens: {
-        '2xl': '1536px',
-        '4xl': '2000px',
+        "2xl": "1536px",
+        "4xl": "2000px",
       },
     },
   },
