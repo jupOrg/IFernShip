b591895d6bc14b82cfc9e536af232cd70a93d95d
yolisses <iamurise@gmail.com>
2023-11-27 15:27:35 -0300

    add cors and json in server

--- alternativeBackend/src/server.ts
+++ alternativeBackend/src/server.ts
@@ -1,8 +1,10 @@
+import cors from "cors";
+import express from "express";
 import "./env";
 
-import express from "express";
-
 const app = express();
+app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.json({ hello: "world" });
