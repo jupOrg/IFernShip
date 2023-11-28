560dba5a9e6dbea15efb06a1b51928e1df315768
yolisses <iamurise@gmail.com>
2023-11-28 08:57:30 -0300

    add mongo docker compose

--- backend/database/index.js
+++ backend/database/index.js
@@ -1,10 +1,8 @@
 import mongoose from "mongoose";
-import { MONGODB_HOST, MONGODB_PASSWORD, MONGODB_USER } from "../env.js";
-
-const connectionString = `mongodb+srv://${MONGODB_USER}:${MONGODB_PASSWORD}@${MONGODB_HOST}/?retryWrites=true&w=majority`;
+import { DATABASE_URL } from "../env.js";
 
 async function connect() {
-  await mongoose.connect(connectionString);
+  await mongoose.connect(DATABASE_URL);
 }
 
 export default connect;
--- backend/env.js
+++ backend/env.js
@@ -1,4 +1,4 @@
 import { config } from "dotenv";
 config();
 
-export const { MONGODB_USER, MONGODB_PASSWORD, MONGODB_HOST } = process.env;
+export const { DATABASE_URL } = process.env;
