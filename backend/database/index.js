3f56641cdfec7316ee288c06f47a88da3d7e8dcf
yolisses <iamurise@gmail.com>
2023-11-28 08:47:32 -0300

    fix database imports

--- backend/database/index.js
+++ backend/database/index.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import { MONGODB_HOST, MONGODB_PASSWORD, MONGODB_USER } from "../env.js";
 
 const connectionString = `mongodb+srv://${MONGODB_USER}:${MONGODB_PASSWORD}@${MONGODB_HOST}/?retryWrites=true&w=majority`;
 
