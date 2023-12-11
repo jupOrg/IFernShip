948e6e14d41ba9e18ac5eb2eaf069d4c2adf0ff5
Pedro <pedrojuniorifpb@gmail.com>
2023-12-11 19:03:15 -0300

    logic for register interneship add whit success

--- frontend/src/internship/createInternshipPage.tsx
+++ frontend/src/internship/createInternshipPage.tsx
@@ -1,10 +1,84 @@
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+import * as yup from "yup";
+
 import { GoBackArrow } from "../common/goBackArrow";
 import { GradientCurve } from "../common/gradientCurve";
+import { useEffect, useState } from "react";
+import { Enterprise } from "../types/enterprise";
+import { createApiInstance } from "../api/api";
+import { useAuth } from "../auth/authContext";
+import { Internship } from "../types/internship";
+
+type Values = Omit<Internship, "id" | "enterprise" | "isActive">;
+type FieldValues = Values & { enterpriseId: string };
+
+const schema = yup.object({
+  profissional_profile: yup
+    .string()
+    .required("É requerido repassar um perfil de profissional"),
+  description: yup
+    .string()
+    .required("É requerido repassar um perfil de profissional"),
+  work_style: yup
+    .string()
+    .required("Selectione um estilo de trabalho")
+    .oneOf(
+      ["isPerson", "remote", "hibrid"],
+      "Selecione um estilo de trabalho válido"
+    ),
+  course: yup.string().required("Selecione um curso"),
+  office: yup.string().required("É nescessario repassar o Cargo"),
+  weekly_workload: yup
+    .number()
+    .required("É nescessario passar a quantidade de horas trabalhadas")
+    .positive("A carga horária deve ser um número positivo"),
+  enterpriseId: yup.string().required("Selecione uma empresa"),
+});
 
 export function CreateInternshipPage() {
-  // TODO load real companies
-  const companies = ["company 1", "company 2", "company 3"];
-  const courses = ["course 1", "course 2", "course 3"];
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FieldValues>({
+    resolver: yupResolver(schema),
+  });
+  const [companies, setCompanies] = useState<Enterprise[]>([]);
+  const courses = [
+    "ADS",
+    "Engenharia e Controle de Automação",
+    "Engenharia Civil",
+    "Matemática",
+  ];
+
+  const { token, handleModal, closeModal } = useAuth();
+
+  const api = createApiInstance(token);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const main = async () => {
+      const res = await api.get("/enterprises");
+      setCompanies(res.data);
+    };
+    main();
+  }, []);
+
+  async function submit(interneship: FieldValues) {
+    const res = await api.post("/internships", interneship);
+    if (res.status === 201) {
+      handleModal?.({
+        isVisible: true,
+        title: "Empressa registrada com Sucesso",
+        callbackClose: () => {
+          navigate("/");
+          closeModal();
+        },
+      });
+    }
+  }
 
   return (
     <div>
@@ -15,30 +89,28 @@ export function CreateInternshipPage() {
             <GoBackArrow to="/estagios" />
             Cadastrar estágio
           </h1>
-          <form className="gap-2 flex flex-col">
+          <form className="gap-2 flex flex-col" onSubmit={handleSubmit(submit)}>
             <textarea
               rows={3}
               className="default-input"
               placeholder="Perfil do profissional"
+              {...register("profissional_profile")}
             />
             <textarea
               rows={3}
               className="default-input"
-              placeholder="Perfil da vaga"
+              placeholder="Descrição da Vaga"
+              {...register("description")}
             />
-            <input
-              type="text"
-              className="default-input"
-              placeholder="Benefícios"
-            />
-            <select className="default-input">
+            <select className="default-input" {...register("work_style")}>
               <option value="" disabled selected>
                 Estilo de trabalho
               </option>
               <option value="isPerson">Presencial</option>
+              <option value="hibrid">Hibrido</option>
               <option value="remote">Remoto</option>
             </select>
-            <select className="default-input">
+            <select className="default-input" {...register("course")}>
               <option value="" disabled selected>
                 Curso
               </option>
@@ -46,18 +118,24 @@ export function CreateInternshipPage() {
                 <option value={course}>{course}</option>
               ))}
             </select>
-            <input type="text" className="default-input" placeholder="Cargo" />
+            <input
+              type="text"
+              className="default-input"
+              placeholder="Cargo"
+              {...register("office")}
+            />
             <input
               type="number"
               className="default-input"
               placeholder="Carga horária semanal"
+              {...register("weekly_workload")}
             />
-            <select className="default-input">
+            <select className="default-input" {...register("enterpriseId")}>
               <option value="" disabled selected>
                 Empresa
               </option>
               {companies.map((company) => (
-                <option value={company}>{company}</option>
+                <option value={company.id}>{company.name}</option>
               ))}
             </select>
             <button type="submit" className="default-submit">
--- frontend/src/types/internship.d.ts
+++ frontend/src/types/internship.d.ts
@@ -2,7 +2,12 @@ import { Enterprise } from "./enterprise";
 
 export type Internship = {
   id: string;
-  position: string;
+  office: string;
   isActive: boolean;
   enterprise: Enterprise;
+  work_style: 'isPerson' | 'remote' | 'hibrid';
+  profissional_profile: string;
+  weekly_workload: number;
+  description: string;
+  course: string;
 };
