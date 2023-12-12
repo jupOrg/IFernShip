c4ae1eca7a97057a7ab3c6953ceb6855ea7c523a
yolisses <iamurise@gmail.com>
2023-12-11 22:42:06 -0300

    add spinner

--- frontend/src/internship/internshipsPage.tsx
+++ frontend/src/internship/internshipsPage.tsx
@@ -1,17 +1,17 @@
 import { useEffect, useState } from "react";
 import { createApiInstance } from "../api/api";
+import { useAuth } from "../auth/authContext";
 import { GradientCurve } from "../common/gradientCurve";
 import { NavBar } from "../nav/navBar";
 import { TopBar } from "../nav/topBar";
 import { Internship } from "../types/internship";
 import { InternshipsItem } from "./internshipItem";
-import { useAuth } from "../auth/authContext";
-import { SplashPage } from "../intro/splashPage";
+import { LoadingPlaceholder } from "../common/loadingPlaceholder";
 
 export function InternshipsPage() {
   const [internships, setInternships] = useState<Internship[]>();
 
-  const { token } = useAuth()
+  const { token } = useAuth();
   const api = createApiInstance(token);
 
   async function getInternships() {
@@ -23,22 +23,23 @@ export function InternshipsPage() {
     getInternships();
   }, []);
 
-  // TODO replace this
-  if (!internships) return <SplashPage />;
-
   return (
     <div className="flex flex-row min-h-screen">
       <GradientCurve />
       <NavBar />
       <div className="items-center p-2 gap-4 flex-1">
         <TopBar />
-        <div className="sm:grid sm:grid-cols-2 lg:grid-cols-3 gap-2 w-full max-w-4xl">
-          {internships.map((internship) => {
-            return (
-              <InternshipsItem internship={internship} key={internship.id} />
-            );
-          })}
-        </div>
+        {internships ? (
+          <div className="sm:grid sm:grid-cols-2 lg:grid-cols-3 gap-2 w-full max-w-4xl ">
+            {internships.map((internship) => {
+              return (
+                <InternshipsItem internship={internship} key={internship.id} />
+              );
+            })}
+          </div>
+        ) : (
+          <LoadingPlaceholder />
+        )}
       </div>
     </div>
   );
