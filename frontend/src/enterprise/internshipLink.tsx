6408dad781f4ca9701d2a2cf52bf4adb75b6f1d4
yolisses <iamurise@gmail.com>
2023-12-12 11:38:18 -0300

    copy-paste the internshipItem for internshipLink

--- frontend/src/enterprise/internshipLink.tsx
+++ frontend/src/enterprise/internshipLink.tsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import { IsActiveLabel } from "../internship/isActiveLabel";
 import { Internship } from "../types/internship";
 
 type Props = {
@@ -7,9 +8,12 @@ type Props = {
 
 export function InternshipLink({ internship }: Props) {
   return (
-    <Link to={"/estagios/" + internship.id}>
-      <div>
-        <div>{internship.office}</div>
+    <Link to={`/estagios/${internship.id}`}>
+      <div className="bg-white rounded-xl p-2 hover:shadow-xl transition duration-75">
+        <div className="flex-row justify-between">
+          <div className="font-semibold">{internship.office}</div>
+          <IsActiveLabel isActive={internship.isActive} />
+        </div>
         <div>{internship.description}</div>
       </div>
     </Link>
