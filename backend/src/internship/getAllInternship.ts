3c247c2303c9c3592783c87e5b226a01441b26f5
Pedro <pedrojuniorifpb@gmail.com>
2023-12-11 22:35:53 -0300

    Returnin data enterprise in global router interneships

--- backend/src/internship/getAllInternship.ts
+++ backend/src/internship/getAllInternship.ts
@@ -1,6 +1,12 @@
 import { db } from "../db";
 
 export async function getAllInternship() {
-  const internship = await db.internship.findMany();
+  const internship = await db.internship.findMany({
+    include: {
+      enterprise: {
+        select: { name: true, picture: true, email: true },
+      },
+    },
+  });
   return internship;
 }
