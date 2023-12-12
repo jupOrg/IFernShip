18372b7002f4e95e19d73c25e0e021400b50f531
yolisses <iamurise@gmail.com>
2023-12-12 11:00:43 -0300

    make images bg-black/10 by default

--- frontend/src/common/gradientCurve.tsx
+++ frontend/src/common/gradientCurve.tsx
@@ -3,7 +3,7 @@ export function GradientCurve() {
     <img
       alt=""
       src="/gradient-curve.svg"
-      className="fixed left-0 top-0 bottom-0 h-full -z-10 object-contain hidden md:flex select-none pointer-events-none"
+      className="fixed bg-transparent left-0 top-0 bottom-0 h-full -z-10 object-contain hidden md:flex select-none pointer-events-none"
     />
   );
 }
