0261095f742f50a57dccc3e118dec8999f3f13ee
yolisses <iamurise@gmail.com>
2023-12-12 10:58:56 -0300

    show enterprise data in page

--- frontend/src/enterprise/enterprisePage.tsx
+++ frontend/src/enterprise/enterprisePage.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { api } from "../api/api";
+import { GoBackArrow } from "../common/goBackArrow";
 import { LoadingPlaceholder } from "../common/loadingPlaceholder";
 import { Enterprise } from "../types/enterprise";
 
@@ -9,7 +10,7 @@ export function EnterprisePage() {
   const [enterprise, setEnterprise] = useState<Enterprise>();
 
   async function getEnterprise() {
-    const res = await api.get("/enterprise/" + id);
+    const res = await api.get("/enterprises/" + id);
     setEnterprise(res.data);
   }
 
@@ -22,8 +23,33 @@ export function EnterprisePage() {
   }
 
   return (
-    <div>
-      <h1>{enterprise.name}</h1>
+    <div className="w-full max-w-xl gap-4">
+      <section>
+        <h1 className="page-header">
+          <GoBackArrow to="/empresas" />
+          {enterprise.name}
+        </h1>
+        <img
+          src={enterprise.picture}
+          className="h-72 object-cover rounded-lg bg-black/10"
+        />
+      </section>
+      <section>
+        <h2 className="font-semibold text-xl text-green-700">
+          Sobre a empresa
+        </h2>
+        <div>{enterprise.description}</div>
+      </section>
+      <section>
+        <div className="flex-row gap-2">
+          <h3 className="font-semibold text-green-700">Email:</h3>
+          {enterprise.email}
+        </div>
+        <div className="flex-row gap-2">
+          <h3 className="font-semibold text-green-700">CNPJ:</h3>
+          {enterprise.cnpj}
+        </div>
+      </section>
     </div>
   );
 }
