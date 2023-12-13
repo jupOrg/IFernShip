fc7b3dd0af3b546512a1e69219094a200d961576
yolisses <iamurise@gmail.com>
2023-12-13 14:14:11 -0300

    use ifpb-logo-black in logo.tsx

--- frontend/src/common/logo.tsx
+++ frontend/src/common/logo.tsx
@@ -3,7 +3,11 @@ import { Link } from "react-router-dom";
 export function Logo() {
   return (
     <Link to="/" className="fixed hidden sm:flex top-5 left-5">
-      <img alt="logo" src="./ifpb-logo.svg" className="w-14 aspect-square" />
+      <img
+        alt="logo"
+        src="./ifpb-logo-black.svg"
+        className="w-14 aspect-square"
+      />
     </Link>
   );
 }
