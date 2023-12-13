cc06ce5e3e8e612e2503c09ba33c8b9f405fe317
yolisses <iamurise@gmail.com>
2023-12-12 22:35:37 -0300

    fix loadingPlaceholder style

--- frontend/src/common/loadingPlaceholder.tsx
+++ frontend/src/common/loadingPlaceholder.tsx
@@ -1,6 +1,6 @@
 export function LoadingPlaceholder() {
   return (
-    <div className="flex-1 justify-center">
+    <div className="flex-1 justify-center items-center">
       <img src="/spinner.svg" alt="" className="animate-spin w-10" />
     </div>
   );
