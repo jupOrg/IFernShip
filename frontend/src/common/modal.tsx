87389d3df93e070f1487c5bd2c6b68d49a267239
yolisses <iamurise@gmail.com>
2023-12-12 22:58:10 -0300

    simplify modal

--- frontend/src/common/modal.tsx
+++ frontend/src/common/modal.tsx
@@ -11,27 +11,18 @@ export function Modal({
   isVisible,
   callbackClose,
 }: ModalProps) {
-  const classVisible = isVisible
-    ? "opacity-100 visible"
-    : "invisible opacity-0";
+  const classVisible = isVisible ? "fixed opacity-100" : "hidden opacity-0";
   return (
     <div
       data-cy="modal-error"
-      className={`${classVisible} duration-200 ease-linear bg-black bg-opacity-80 fixed inset-0 min-h-screen flex justify-center items-center transition-modal`}
+      className={`${classVisible} duration-200 ease-linear transition bg-black bg-opacity-80 fixed inset-0 justify-center items-center`}
     >
-      <div className="bg-white px-9 py-10 space-y-9 shadow-common rounded-xl w-[40rem]">
-        <span>
-          <h1 className="text-2xl font-medium">{title}</h1>
-        </span>
-        <span>
-          <p className="text-lg">{message}</p>
-        </span>
-        <div>
-          <button
-            className="btn btn-primary w-1/4 self-end"
-            onClick={callbackClose}
-          >
-            OK
+      <div className="bg-white p-4 gap-2 shadow-common rounded-xl w-full max-w-lg">
+        <h1 className="text-xl font-medium">{title}</h1>
+        <p>{message}</p>
+        <div className="sm:flex-row sm:justify-end">
+          <button className="btn btn-primary px-14" onClick={callbackClose}>
+            Ok
           </button>
         </div>
       </div>
--- frontend/src/dev/devPage.tsx
+++ frontend/src/dev/devPage.tsx
@@ -1,3 +1,21 @@
+import { useState } from "react";
+import { Modal } from "../common/modal";
+
 export function DevPage() {
-  return <div>hello dev</div>;
+  const [isVisible, setIsVisible] = useState(true);
+
+  // useEffect(() => {
+  //   setInterval(() => {
+  //     setIsVisible((old) => !old);
+  //   }, 1000);
+  // }, []);
+
+  return (
+    <div>
+      hello dev
+      <Modal isVisible={isVisible} title="teste" message="algo">
+        <div>teste</div>
+      </Modal>
+    </div>
+  );
 }
